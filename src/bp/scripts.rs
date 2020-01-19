1ccbc8f7d23a94458b43238e4291514b6bb2ed14
Dr. Maxim Orlovsky <orlovsky@pandoracore.com>
2020-01-19 16:15:56 +0100

    WIP pubkey tweaking within miniscript: got to Arc mutability issues

--- src/bp/scripts.rs
+++ src/bp/scripts.rs
@@ -11,6 +11,7 @@
 // along with this software.
 // If not, see <https://opensource.org/licenses/MIT>.
 
+
 use core::marker::PhantomData;
 use bitcoin::{
     hash_types::*,
@@ -50,51 +51,102 @@ impl<Pk: MiniscriptKey> From<miniscript::Error> for LockScriptParseError<Pk> {
     }
 }
 
+
+fn collect_pubkeys(
+    miniscript: &Miniscript<miniscript::bitcoin::PublicKey>
+) -> Result<Vec<bitcoin::PublicKey>, LockScriptParseError<bitcoin::PublicKey>> {
+    Ok(match &miniscript.node {
+        Pk(key) => vec![key.clone()],
+        PkH(pkh) => Err(LockScriptParseError::PubkeyHash(pkh.clone()))?,
+        ThreshM(_, keys) =>
+            keys.iter().map(|key| key.clone()).collect(),
+
+        Alt(node) |
+        Swap(node) |
+        Check(node) |
+        DupIf(node) |
+        Verify(node) |
+        NonZero(node) |
+        ZeroNotEqual(node) =>
+            collect_pubkeys(&node)?,
+        AndV(node1, node2) |
+        AndB(node1, node2) |
+        OrB(node1, node2) |
+        OrD(node1, node2) |
+        OrC(node1, node2) |
+        OrI(node1, node2) =>
+            collect_pubkeys(&node1)?.into_iter()
+                .chain(collect_pubkeys(&node2)?.into_iter()).collect(),
+        AndOr(node1, node2, node3) =>
+            collect_pubkeys(&node1)?.into_iter()
+                .chain(collect_pubkeys(&node2)?.into_iter())
+                .chain(collect_pubkeys(&node3)?.into_iter()).collect(),
+        Thresh(_, node_vec) => {
+            let mut vec= vec![];
+            for node in node_vec {
+                vec.append(&mut collect_pubkeys(&node)?);
+            }
+            vec
+        }
+
+        _ => vec![],
+    })
+}
+
+
+fn for_each_pubkey<E: Into<LockScriptParseError<bitcoin::PublicKey>>>(
+    miniscript: &mut Miniscript<bitcoin::PublicKey>,
+    f: fn(&mut bitcoin::PublicKey) -> Result<(), E>
+) -> Result<usize, LockScriptParseError<bitcoin::PublicKey>> {
+    Ok(match &mut miniscript.node {
+        Pk(key) => f(key).map_err(|err| err.into()).map(|_| 1)?,
+        PkH(pkh) =>
+            Err(LockScriptParseError::PubkeyHash(pkh.clone()))?,
+        ThreshM(_, keys) =>
+            keys.into_iter()
+                .try_for_each(|mut key| f(&mut key).map_err(|err| err.into()))
+                .map(|_| keys.len())?,
+
+        Alt(node) |
+        Swap(node) |
+        Check(node) |
+        DupIf(node) |
+        Verify(node) |
+        NonZero(node) |
+        ZeroNotEqual(node) =>
+            for_each_pubkey(node, f)?,
+        AndV(node1, node2) |
+        AndB(node1, node2) |
+        OrB(node1, node2) |
+        OrD(node1, node2) |
+        OrC(node1, node2) |
+        OrI(node1, node2) =>
+            for_each_pubkey(node1, f)? +
+            for_each_pubkey(node2, f)?,
+        AndOr(node1, node2, node3) =>
+            for_each_pubkey(node1, f)? +
+            for_each_pubkey(node2, f)? +
+            for_each_pubkey(node3, f)?,
+        Thresh(_, node_vec) =>
+            node_vec.into_iter()
+                .try_fold(0, |acc, node|
+                    for_each_pubkey(node, f)
+                )?,
+
+        _ => 0,
+    })
+}
+
+
 impl LockScript {
     pub fn extract_pubkeys(&self) -> Result<Vec<secp256k1::PublicKey>, LockScriptParseError<bitcoin::PublicKey>> {
         let miniscript = Miniscript::parse(&self.clone().into_inner())?;
-        Ok(Self::extract_pubkeys_from_terminal(&miniscript)?.iter().map(|bpk| bpk.key).collect())
+        Ok(collect_pubkeys(&miniscript)?.iter().map(|bpk| bpk.key).collect())
     }
 
-    fn extract_pubkeys_from_terminal(
-        miniscript: &Miniscript<miniscript::bitcoin::PublicKey>
-    ) -> Result<Vec<bitcoin::PublicKey>, LockScriptParseError<bitcoin::PublicKey>> {
-        Ok(match &miniscript.node {
-            Pk(key) => vec![key.clone()],
-            PkH(pkh) => Err(LockScriptParseError::PubkeyHash(pkh.clone()))?,
-            ThreshM(_, keys) =>
-                keys.iter().map(|key| key.clone()).collect(),
-
-            Alt(node) |
-            Swap(node) |
-            Check(node) |
-            DupIf(node) |
-            Verify(node) |
-            NonZero(node) |
-            ZeroNotEqual(node) =>
-                Self::extract_pubkeys_from_terminal(&node)?,
-            AndV(node1, node2) |
-            AndB(node1, node2) |
-            OrB(node1, node2) |
-            OrD(node1, node2) |
-            OrC(node1, node2) |
-            OrI(node1, node2) =>
-                Self::extract_pubkeys_from_terminal(&node1)?.into_iter()
-                    .chain(Self::extract_pubkeys_from_terminal(&node2)?.into_iter()).collect(),
-            AndOr(node1, node2, node3) =>
-                Self::extract_pubkeys_from_terminal(&node1)?.into_iter()
-                    .chain(Self::extract_pubkeys_from_terminal(&node2)?.into_iter())
-                    .chain(Self::extract_pubkeys_from_terminal(&node3)?.into_iter()).collect(),
-            Thresh(_, node_vec) => {
-                let mut vec= vec![];
-                for node in node_vec {
-                    vec.append(&mut Self::extract_pubkeys_from_terminal(&node)?);
-                }
-                vec
-            }
-
-            _ => vec![],
-        })
+    pub fn replace_pubkeys<E: Into<LockScriptParseError<bitcoin::PublicKey>>>(&mut self, f: fn(&mut bitcoin::PublicKey) -> Result<(), E>) -> Result<usize, LockScriptParseError<bitcoin::PublicKey>> {
+        let mut miniscript = Miniscript::parse(&self.clone().into_inner())?;
+        for_each_pubkey(&mut miniscript, f)
     }
 }
 
