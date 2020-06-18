f2f43ce61405ab2ee9b8dbd585dcede7d655a779
Dr. Maxim Orlovsky <orlovsky@pandoracore.com>
2020-06-18 17:08:39 +0200

    WIP on RGB finalization

--- src/lnpbps/lnpbp4.rs
+++ src/lnpbps/lnpbp4.rs
@@ -12,18 +12,20 @@
 // If not, see <https://opensource.org/licenses/MIT>.
 
 use crate::commit_verify::CommitVerify;
-use bitcoin::hashes::{sha256, sha256d, Hash, HashEngine};
+use bitcoin::hashes::{sha256, Hash, HashEngine};
 use rand::Rng;
+use std::collections::btree_map::Entry;
 use std::collections::BTreeMap;
 
-/// Source data for creation of multimessage commitments according to LNPBP-4 procedure
-type MultiMsg = BTreeMap<u64, sha256::Hash>;
+/// Source data for creation of multi-message commitments according to LNPBP-4 procedure
+pub type MultiMsg = BTreeMap<u64, sha256::Hash>;
+pub type Lnpbp4Hash = sha256::Hash;
 
 /// Multimessage commitment data according to LNPBP-4 specification
 #[derive(Clone, PartialEq, Eq, PartialOrd, Ord, Debug, Display)]
 #[display_from(Debug)]
 pub struct MultimsgCommitment {
-    data: Vec<u8>,
+    data: Vec<Lnpbp4Hash>,
     entropy: u64,
 }
 
@@ -32,9 +34,9 @@ impl CommitVerify<MultiMsg> for MultimsgCommitment {
         const SORT_LIMIT: u64 = 2 << 16;
 
         let mut n = multimsg.len() as u64;
-        let ordered = loop {
+        let mut ordered = loop {
             let mut ordered = MultiMsg::new();
-            // TODO: Modify arythmetics in LNPBP-4 spec
+            // TODO: Modify arithmetics in LNPBP-4 spec
             if multimsg
                 .into_iter()
                 .all(|(sort_code, digest)| ordered.insert(sort_code % n, digest.clone()).is_none())
@@ -44,7 +46,7 @@ impl CommitVerify<MultiMsg> for MultimsgCommitment {
             n += 1;
             if n > SORT_LIMIT {
                 panic!(
-                    "Memory allocation limit exceeded while trying to sort multimessage commitment"
+                    "Memory allocation limit exceeded while trying to sort multi-message commitment"
                 );
             }
         };
@@ -58,16 +60,16 @@ impl CommitVerify<MultiMsg> for MultimsgCommitment {
             engine.input(&entropy.to_le_bytes());
             sha256::Hash::from_engine(engine)
         };
-        let mut data: Vec<u8> = vec![];
+        let mut data: Vec<Lnpbp4Hash> = vec![];
         for i in 1..=n {
-            match ordered.get(&i) {
-                Some(digest) => data.extend_from_slice(&digest[..]),
-                None => {
-                    let mut engine = sha256d::Hash::engine();
+            match ordered.entry(i) {
+                Entry::Vacant(_) => {
+                    let mut engine = sha256::Hash::engine();
                     engine.input(&i.to_le_bytes());
                     engine.input(&entropy_digest[..]);
-                    data.extend_from_slice(&sha256d::Hash::from_engine(engine)[..])
+                    data.push(sha256::Hash::from_engine(engine))
                 }
+                Entry::Occupied(entry) => data.push(entry.remove()),
             }
         }
         Self { data, entropy }
--- src/rgb/anchor.rs
+++ src/rgb/anchor.rs
@@ -13,10 +13,24 @@
 
 use std::io;
 
+use bitcoin::Transaction;
+
+use crate::bp::OuterWitness;
+use crate::lnpbp4::{Lnpbp4Hash, MultimsgCommitment};
 use crate::strict_encoding::{self, StrictDecode, StrictEncode};
 
 #[derive(Clone, Debug)]
-pub struct Anchor {}
+pub struct Anchor {
+    pub commitments: Vec<Lnpbp4Hash>,
+    pub entropy: Option<u64>,
+    pub proof: OuterWitness,
+}
+
+impl Anchor {
+    pub fn new(_lnpbp4: MultimsgCommitment, _tx: &Transaction) -> Self {
+        unimplemented!()
+    }
+}
 
 impl StrictEncode for Anchor {
     fn strict_encode<E: io::Write>(&self, _: E) -> Result<usize, strict_encoding::Error> {
