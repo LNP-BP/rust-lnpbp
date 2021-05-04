1d171ab173efe232ab61785db3a6c4b2d98d82e9
Dr Maxim Orlovsky <orlovsky@pandoracore.com>
2021-05-04 13:44:31 +0200

    WIP on refactoring strict_encoding_derive

--- strict_encoding/derive/examples/test.rs
+++ strict_encoding/derive/examples/test.rs
@@ -1,4 +1,4 @@
-#![allow(dead_code, bare_trait_objects)]
+#![allow(dead_code)]
 
 use strict_encoding::{StrictDecode, StrictEncode};
 
@@ -26,6 +26,7 @@ struct Other {
 }
 
 #[derive(StrictEncode, StrictDecode)]
+#[strict_encoding_crate(strict_encoding)]
 enum Hi {
     /// Docstring
     First(u8),
@@ -38,6 +39,16 @@ enum Hi {
 }
 
 #[derive(StrictEncode, StrictDecode)]
+#[strict_encoding_enum(value)]
+#[repr(u8)]
+enum ByValue {
+    Bit8 = 1,
+    Bit16 = 2,
+    Bit32 = 4,
+    Bit64 = 8,
+}
+
+#[derive(StrictEncode, StrictDecode)]
 enum CustomErr<Err>
 where
     Err: std::error::Error + StrictEncode + StrictDecode,
@@ -45,4 +56,6 @@ where
     Other(Err),
 }
 
-fn main() {}
+fn main() {
+    assert_eq!(ByValue::Bit64.strict_serialize().unwrap(), vec![8])
+}
