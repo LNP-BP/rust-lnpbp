9ebb8c1cc67554b85cf66e9c01d67e49dd40e743
Dr. Maxim Orlovsky <orlovsky@pandoraprime.ch>
2022-11-20 02:23:18 +0100

    Further work on LNP/BP identity certificates

--- identity/src/lib.rs
+++ identity/src/lib.rs
@@ -14,8 +14,9 @@
 #[macro_use]
 extern crate amplify;
 
-use std::fmt::{self, Display, Formatter};
+use std::fmt::{self, Debug, Display, Formatter};
 use std::io::Write;
+use amplify::hex::ToHex;
 use bech32::ToBase32;
 use bitcoin_hashes::sha256;
 use strict_encoding::{Error, StrictEncode};
@@ -128,11 +129,17 @@ impl EdComp {
     }
 }
 
-#[derive(Clone, Ord, PartialOrd, Eq, PartialEq, Hash, Debug)]
+#[derive(Clone, Ord, PartialOrd, Eq, PartialEq, Hash)]
 pub struct Certificate {
     algo: Algo,
     pubkey: Box<[u8]>,
     sig: Box<[u8]>
+}
+
+impl Certificate {
+    pub fn fingerprint(&self) -> String {
+        format!("{:#}", self)
+    }
 }
 
 impl StrictEncode for Certificate {
@@ -144,13 +151,45 @@ impl StrictEncode for Certificate {
     }
 }
 
+impl Debug for Certificate {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        fn char4ws(v: &[u8]) -> String {
+            v.to_hex()
+                .chars()
+                .enumerate()
+                .flat_map(|(i, c)| {
+                    if i != 0 && i % 4 == 0 {
+                        Some(' ')
+                    } else {
+                        None
+                    }
+                        .into_iter()
+                        .chain(std::iter::once(c))
+                })
+                .collect::<String>()
+        }
+
+        writeln!(f, "fgp   {}", self.fingerprint())?;
+        writeln!(f, "alg   {}", self.algo)?;
+        writeln!(f, "idk   {}", char4ws(&self.pubkey))?;
+        writeln!(f, "sig   {}", char4ws(&self.sig))?;
+
+        Ok(())
+    }
+}
+
 impl Display for Certificate {
     fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
         let data = self.strict_serialize()
             .expect("strict encoding of certificate");
         let s = bech32::encode("crt", data.to_base32(), bech32::Variant::Bech32m)
             .expect("bech32 encoding of certificate");
-        f.write_str(&s)?;
+
+        if f.alternate() {
+            f.write_str(&s[s.len() - 6..])?;
+        } else {
+            f.write_str(&s)?;
+        }
         f.write_str("_")?;
 
         let mut mnemonic = Vec::with_capacity(64);
@@ -179,13 +218,22 @@ impl From<secp256k1::KeyPair> for Certificate {
 
 #[cfg(test)]
 mod test {
-    use secp256k1::{rand, SECP256K1};
+    use secp256k1::{SECP256K1};
     use crate::Certificate;
 
     #[test]
     fn test() {
-        let pair = secp256k1::KeyPair::new(&SECP256K1, &mut rand::thread_rng());
+        let pair = secp256k1::KeyPair::from_seckey_slice(&SECP256K1, &secp256k1::ONE_KEY[..]).unwrap();
         let cert = Certificate::from(pair);
-        println!("{}", cert);
+        /*
+        assert_eq!(format!("{}", cert), "crt1qyghn0nx0muaewav2ksx99wwsu9swq5mlndjmn3gm9vl9q2mzmup0xrdgswg9ate53t5hvppkl2xjem0y2sg5r738s7jqdlk4jd49v72c4t0f7e3a2yup6xhldv4c35hf5ncvas3r8ulwf4xx3ynqy3vwsc37avgyrl_game_accent_candle");
+        assert_eq!(format!("{:#}", cert), "8wdwat_game_accent_candle");
+        assert_eq!(format!("{:?}", cert), "\
+        fgp   gsjc3l_game_accent_candle
+        alg   secp256k1-bip340-xonly
+        idk   79be 667e f9dc bbac 55a0 6295 ce87 0b07 029b fcdb 2dce 28d9 59f2 815b 16f8 1798
+        sig   e4b4 1a37 317e 1de6 bd00 ec17 fcb6 940a cda0 5a21 586c 3134 b793 c0c2 d89f 8e09 2efc c7af 9829 78a0 9f19 8040 e680 66e2 859c 4a8e 8036 2d4e e659 5333 388e 6949
+        ");
+         */
     }
 }
